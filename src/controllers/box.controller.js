9f062295d13e9d62e3fe2188e31406bd823eba0b
Daniela <dannytorres0211@gmail.com>
2022-07-20 19:47:45 -0500

    Change on box controller

--- src/controllers/box.controller.js
+++ src/controllers/box.controller.js
@@ -15,7 +15,7 @@ module.exports = {
   async show(req, res) {
     try {
       const { boxId } = req.params;
-      const box = await Box.findById(boxId).populate("productsRef");
+      const box = await Box.findById(boxId).populate("products");
       res.status(200).json({ message: "Box found", data: box });
     } catch (err) {
       res.status(404).json({ message: "Box not found" });
